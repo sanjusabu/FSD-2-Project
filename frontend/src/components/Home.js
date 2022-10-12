463d8d1ce85db9f44a7d44cfdfee4e89c2b1947f
sanju <sanjusabu066@gmail.com>
2022-10-12 11:53:41 +0530

    same

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,34 @@
+import NavBar from "./NavBar";
+import Cards from "./Cards";
+import SimpleImageSlider from "react-simple-image-slider";
+import useResizeObserver from "use-resize-observer";
+const Home =()=>{
+    const { ref, width = 1, height = 1 } = useResizeObserver();
+const images = [
+    { url: "stocks.jpg" },
+    { url: "stocks.jpg" },
+    { url: "stocks.jpg" },
+    { url: "stocks.jpg" },
+  ];
+    return (
+        <>
+        <NavBar />
+        <div ref={ref} className="card_imgBox" style={{ width: "100%", height: "70vh" }} >
+        <SimpleImageSlider width={896} height={500} images={images} showBullets={true} showNavs={true} />
+        </div>          
+                  <Cards text="Some quick example text to build on the card title and make up the
+          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
+       
+    
+                  <Cards text="Some quick example text to build on the card title and make up the
+          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
+             
+                <Cards text="Some quick example text to build on the card title and make up the
+          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
+
+        </>
+    )
+
+}
+
+export default Home;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../../context/authcontext";
+import { useContext } from "react";
+const NavBar = () => {
+//   const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+//   const logoutHandler = () => {
+//     if (auth.isLoggedIn) {
+//       auth.logout();
+//       localStorage.removeItem("userid");
+//       navigate("/Home");
+//     }
+//   };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="#">
+            SPD
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+{/*                 
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/Landingpage"
+                  >
+                    Home
+                  </Link> */}
+                
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                
+              </li>
+              {/* <li className="nav-item">
+                <Link className="nav-link active" to="/profile">
+                  Profile
+                </Link>
+              </li> */}
+        
+            </ul>
+            {/* <li className="nav-item loggout">
+              <button className="btn btn-danger logg" onClick={logoutHandler}>
+                Logout
+              </button>
+            </li> */}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
