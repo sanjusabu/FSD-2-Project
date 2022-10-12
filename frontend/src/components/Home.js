88696295a0f62a247cf1537435d876dfe6abb2a3
PJ0710 <pranjay.g20@iiits.in>
2022-10-12 19:39:42 +0530

    cardgroup

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,34 +1,56 @@
 import NavBar from "./NavBar";
 import Cards from "./Cards";
+import CardGroup from "react-bootstrap/CardGroup";
 import SimpleImageSlider from "react-simple-image-slider";
 import useResizeObserver from "use-resize-observer";
-const Home =()=>{
-    const { ref, width = 1, height = 1 } = useResizeObserver();
-const images = [
+const Home = () => {
+  const { ref, width = 1, height = 1 } = useResizeObserver();
+  const images = [
     { url: "stocks.jpg" },
     { url: "stocks.jpg" },
     { url: "stocks.jpg" },
     { url: "stocks.jpg" },
   ];
-    return (
-        <>
-        <NavBar />
-        <div ref={ref} className="card_imgBox" style={{ width: "100%", height: "70vh" }} >
-        <SimpleImageSlider width={896} height={500} images={images} showBullets={true} showNavs={true} />
-        </div>          
-                  <Cards text="Some quick example text to build on the card title and make up the
-          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
-       
-    
-                  <Cards text="Some quick example text to build on the card title and make up the
-          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
-             
-                <Cards text="Some quick example text to build on the card title and make up the
-          bulk of the card's content." title="Card Title" img="stocks.jpg"/>
+  return (
+    <>
+      <NavBar />
+      <div
+        ref={ref}
+        className="card_imgBox"
+        style={{ width: "100%", height: "70vh" }}
+      >
+        <SimpleImageSlider
+          width={896}
+          height={500}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+        />
+      </div>
+      <CardGroup>
+      <Cards
+        text="Some quick example text to build on the card title and make up the
+          bulk of the card's content."
+        title="Card Title"
+        img="stocks.jpg"
+      />
 
-        </>
-    )
+      <Cards
+        text="Some quick example text to build on the card title and make up the
+          bulk of the card's content."
+        title="Card Title"
+        img="stocks.jpg"
+      />
 
-}
+      <Cards
+        text="Some quick example text to build on the card title and make up the
+          bulk of the card's content."
+        title="Card Title"
+        img="stocks.jpg"
+      />
+      </CardGroup>
+    </>
+  );
+};
 
 export default Home;
