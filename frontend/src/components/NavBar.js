df365fc07c2f342438bc6a6e233cff63c990ede7
PJ0710 <pranjay.g20@iiits.in>
2022-10-27 16:46:52 +0530

    home page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,22 +2,24 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import { AuthContext } from "../../context/authcontext";
 import { useContext } from "react";
+import "./Navbar.css";
+
 const NavBar = () => {
-//   const auth = useContext(AuthContext);
+  //   const auth = useContext(AuthContext);
   const navigate = useNavigate();
-//   const logoutHandler = () => {
-//     if (auth.isLoggedIn) {
-//       auth.logout();
-//       localStorage.removeItem("userid");
-//       navigate("/Home");
-//     }
-//   };
+  //   const logoutHandler = () => {
+  //     if (auth.isLoggedIn) {
+  //       auth.logout();
+  //       localStorage.removeItem("userid");
+  //       navigate("/Home");
+  //     }
+  //   };
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark sticky-nav bg-*">
         <div className="container-fluid">
           <Link className="navbar-brand" to="#">
-            SPD
+            <span className="logo">S.P.D</span>
           </Link>
           <button
             className="navbar-toggler"
@@ -33,7 +35,7 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-{/*                 
+                {/*                 
                   <Link
                     className="nav-link active"
                     aria-current="page"
@@ -41,18 +43,16 @@ const NavBar = () => {
                   >
                     Home
                   </Link> */}
-                
-                  <Link className="nav-link active" aria-current="page" to="/">
-                    Home
-                  </Link>
-                
+
+                <Link className="nav-link active" aria-current="page" to="/">
+                <span className="text">Home</span>
+                </Link>
               </li>
               {/* <li className="nav-item">
                 <Link className="nav-link active" to="/profile">
                   Profile
                 </Link>
               </li> */}
-        
             </ul>
             {/* <li className="nav-item loggout">
               <button className="btn btn-danger logg" onClick={logoutHandler}>
