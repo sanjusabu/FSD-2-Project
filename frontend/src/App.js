f487738da6b5c47635a6de0baec58a51e58f18fb
srikar <saisrikarreddy.k20@iiits.in>
2022-10-28 19:56:44 +0530

    help page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Home from './components/Home';
+import Help from "./components/Help";
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        {/* <Route path='/login' element={<Home />}></Route> */}
-        {/* <Route path='/register' element={<Home />}></Route> */}
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/help" element={<Help />}></Route>
+          {/* <Route path='/login' element={<Home />}></Route> */}
+          {/* <Route path='/register' element={<Home />}></Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
