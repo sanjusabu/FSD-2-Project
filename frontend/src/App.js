7e46a90b59055489ca57d8fcd9462e303546cc7b
srikar <saisrikarreddy.k20@iiits.in>
2022-10-28 19:59:46 +0530

    changes conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
-import Home from './components/Home';
-import Help from "./components/Help";
-=======
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -18,7 +11,6 @@ import { AuthContext } from "./context/auth-context";
 import { useContext, useEffect, useState, useCallback } from "react";
 import Login from "./components/auth/Login";
 
->>>>>>> aa0931d3b0e5b78d0022a5726e10d6bbf01f1e5b
 function App() {
   const details = [];
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -68,17 +60,6 @@ function App() {
   }
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/help" element={<Help />}></Route>
-          {/* <Route path='/login' element={<Home />}></Route> */}
-          {/* <Route path='/register' element={<Home />}></Route> */}
-        </Routes>
-      </Router>
-=======
       <AuthContext.Provider
         value={{
           isLoggedIn: isLoggedIn,
@@ -91,7 +72,6 @@ function App() {
           <main>{routes}</main>
         </Detailscontext.Provider>
       </AuthContext.Provider>
->>>>>>> aa0931d3b0e5b78d0022a5726e10d6bbf01f1e5b
     </div>
   );
 }
