f65408167d24c66b8f451be3ce8252ed886282b8
PJ0710 <pranjay.g20@iiits.in>
2022-10-29 19:23:28 +0530

    portfolio form design

--- frontend/src/components/portfolio/portfolioForm.js
+++ frontend/src/components/portfolio/portfolioForm.js
@@ -1,6 +1,7 @@
 import useInput from "../../hooks/useInput";
 import { useEffect, useState, useContext } from "react";
 import { Portfoliocontext } from "../../context/portfolio-context";
+import "./portfolioForm.css";
 
 const isNotEmpty = (value) => value.trim() !== "";
 const PortfolioForm = (props) => {
@@ -60,51 +61,108 @@ const PortfolioForm = (props) => {
     resettype();
     resetopeningDate();
   };
-  return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <label for="name" style={{ color: "black" }}>
-          Name of Portfolio
-        </label>
-        <input id="name" type="text" value={portName} onChange={portChange} />
-      </div>
 
-      <div>
-        <label for="platform" style={{ color: "black" }}>
-          Platform
-        </label>
-        <input
-          id="platform"
-          type="text"
-          value={platformName}
-          onChange={platformChange}
-        />
+  const [page, setPage] = useState(0);
+
+  const FormTitles = ["Portfolio Name", "Platform", "Type", "Opening Date"];
+
+  const PageDisplay = () => {
+    if (page === 0) {
+      return (
+        <div>
+          <input
+            id="name"
+            type="text"
+            value={portName}
+            placeholder="Name of Portfolio"
+            onChange={portChange}
+          />
+        </div>
+      );
+    } else if (page === 1) {
+      return (
+        <div>
+          <input
+            id="platform"
+            type="text"
+            placeholder="Platform"
+            value={platformName}
+            onChange={platformChange}
+          />
+        </div>
+      );
+    } else if (page === 2) {
+      return (
+        <div>
+          <select id="type" value={type} onChange={typeChange}>
+            <option>Select Type Of Portfolio</option>
+            <option value="Equity">Equity</option>
+            <option value="Cryptocurrency">Cryptocurrency</option>
+          </select>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            id="openingDate"
+            type="date"
+            placeholder="Opening Date"
+            value={openingDate}
+            onChange={openingDateChange}
+          />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="portform">
+      <div className="progressbar">
+        <div
+          style={{
+            width:
+              page === 0
+                ? "25%"
+                : page == 1
+                ? "50%"
+                : page == 2
+                ? "75%"
+                : "100%",
+          }}
+        ></div>
       </div>
-      <div>
-        <label for="type" style={{ color: "black" }}>
-          Type Of Portfolio
-        </label>
-        <select id="type" value={type} onChange={typeChange}>
-          <option>Select Type Of Portfolio</option>
-          <option value="Equity">Equity</option>
-          <option value="Cryptocurrency">Cryptocurrency</option>
-        </select>
+      <div className="portform-container">
+        <form onSubmit={submitHandler}>
+          <div className="header">
+            <h1>{FormTitles[page]}</h1>
+          </div>
+          <div className="body">{PageDisplay()}</div>
+          <div className="footer">
+            <button
+              disabled={page == 0}
+              onClick={() => {
+                setPage((currPage) => currPage - 1);
+              }}
+            >
+              Prev
+            </button>
+            <button
+              disabled={page == 3}
+              onClick={() => {
+                setPage((currPage) => currPage + 1);
+              }}
+            >
+              Next
+            </button>
+          </div>
+
+          <button disabled={!formValid} type="submit">
+            Submit
+          </button>
+        </form>
       </div>
-      <div>
-        <label for="openingDate" style={{ color: "black" }}>
-          Opening Date
-        </label>
-        <input
-          id="openingDate"
-          type="date"
-          value={openingDate}
-          onChange={openingDateChange}
-        />
-      </div>
-      <button disabled={!formValid} className="button-20" type="submit">
-        Submit
-      </button>
-    </form>
+    </div>
   );
 };
 
