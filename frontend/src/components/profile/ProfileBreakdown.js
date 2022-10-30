d1a37532440fb04117cf3c2a06f0e2dd85a91a78
sanju <sanjusabu066@gmail.com>
2022-10-31 00:56:57 +0530

    portfolio breakdown

--- frontend/src/components/profile/ProfileBreakdown.js
+++ frontend/src/components/profile/ProfileBreakdown.js
@@ -1,4 +1,36 @@
+import { Chart as ChartJs, Tooltip, Title, ArcElement, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { Detailscontext } from "../../context/details";
+import { useContext } from "react";
+ChartJs.register(Tooltip, Title, ArcElement, Legend);
 const ProfileBreakdown = () => {
+  const dets = useContext(Detailscontext);
+  console.log(dets);
+  let ticker = dets.details.map((data) => {
+    return data.Ticker;
+  });
+
+  let quantity = dets.details.map((data) => {
+    return data.quantity;
+  });
+
+  let array_len = dets.details.length;
+  let colorarray = [];
+
+  for (let i = 0; i < array_len; i++) {
+    colorarray.push("#" + Math.floor(Math.random() * 16777215).toString(16));
+  }
+
+  console.log(colorarray, "djjkekj");
+  const data = {
+    datasets: [
+      {
+        data: quantity,
+        backgroundColor: colorarray,
+      },
+    ],
+    labels: ticker,
+  };
   return (
     <div className="card h-100">
       <div className="card-header">
@@ -7,8 +39,8 @@ const ProfileBreakdown = () => {
         </span>{" "}
         Portfolio Breakdown
       </div>
-      <div className="card-body">
-        <canvas className="chart" id="pb" width="400" height="200"></canvas>
+      <div style={{ width: "40%" }}>
+        <Pie data={data} />
       </div>
     </div>
   );
