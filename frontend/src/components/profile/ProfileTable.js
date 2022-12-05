f0a78ec6eb98db7e1d814a7349a6a9f9fceb7f39
PJ0710 <pranjay.g20@iiits.in>
2022-12-05 17:34:17 +0530

    merge conf

--- frontend/src/components/profile/ProfileTable.js
+++ frontend/src/components/profile/ProfileTable.js
@@ -5,12 +5,6 @@ import { useRequest } from "../../hooks/request-hook";
 
 const ProfileTable = () => {
   const dets = useContext(Detailscontext);
-<<<<<<< HEAD
-  console.log(dets.details);
-  const mode = useSelector((state) => state.darkMode);
-  const { isdarkMode } = mode;
-  
-=======
   const [colors, setColor] = useState("black");
   console.log(dets.details);
   const mode = useSelector((state) => state.darkMode);
@@ -37,7 +31,6 @@ const ProfileTable = () => {
     Details();
   }, []);
   console.log(transData);
->>>>>>> bc80bab72f5e700a0b3e1dfe254c0a74d17e8092
   return (
     <div className="row">
       <div className="col-md-12 mb-3">
@@ -58,7 +51,6 @@ const ProfileTable = () => {
 
                   {transData.map((i) => (
                     <tr>
-<<<<<<< HEAD
                       <td>{i.Portfolio}</td>
                       <td>{i.Date}</td>
                       <td>{i.Ticker}</td>
@@ -66,15 +58,6 @@ const ProfileTable = () => {
                       <td>{i.quantity}</td>
                       <td>{i.price}</td>
                       <td>{i.total}</td>
-=======
-                      <td style={{ color: colors }}>{i.portfolio}</td>
-                      <td style={{ color: colors }}>{i.date}</td>
-                      <td style={{ color: colors }}>{i.ticker}</td>
-                      <td style={{ color: colors }}>{i.action}</td>
-                      <td style={{ color: colors }}>{i.quantity}</td>
-                      <td style={{ color: colors }}>{i.price}</td>
-                      <td style={{ color: colors }}>{i.total}</td>
->>>>>>> bc80bab72f5e700a0b3e1dfe254c0a74d17e8092
                     </tr>
                   ))}
                 </thead>
