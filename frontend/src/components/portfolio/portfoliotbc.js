791a0e5d7eb9922569b5672b7920b06a5643fbe5
sanju <sanjusabu066@gmail.com>
2022-12-05 20:08:15 +0530

    portfoliio table working

--- frontend/src/components/portfolio/portfoliotbc.js
+++ frontend/src/components/portfolio/portfoliotbc.js
@@ -1,31 +1,73 @@
 import { useState, useContext } from "react";
-import { Detailscontext } from "../../context/details";
+// import { Detailscontext } from "../../context/details";
 import { Portfoliocontext } from "../../context/portfolio-context";
 import { useEffect } from "react";
 import { useRequest } from "../../hooks/request-hook";
+import { Transcontext } from "../../context/trans-context";
 
 const Check = (props) => {
   // console.log(props.details, props.count);
-  const port = useContext(Portfoliocontext);
-  const dets = useContext(Detailscontext);
+  // const port = useContext(Portfoliocontext);
+  // const dets = useContext(Detailscontext);
   const [show, setShow] = useState(false);
   const [match, setMatch] = useState([]);
   const [val, setVal] = useState(0);
-  const [images, setImages] = useState([]);
+  // const [images, setImages] = useState([]);
   const { sendRequest } = useRequest();
+  const [portData, setportData] = useState([]);
+  const [transData, setTransData] = useState([]);
+  const trans = useContext(Transcontext);
 
   useEffect(() => {
-    let save = [];
+    const Details = async () => {
+      const res = await sendRequest(
+        "http://localhost:5011/trans/getTrans",
+        "POST",
+        JSON.stringify({
+          id: localStorage.getItem("user"),
+        }),
+        { "Content-Type": "application/json" }
+      );
+      // console.log(res, "getformdata");
+      setTransData(res);
+      // setportData((prevstate)=>{
+      //   let newstate = [...prevstate,]
+      // })
+      // res.map((data) => setportData(data));
+    };
+    Details();
+    // console.log(trans.tr);
+    const Portfoilo = async () => {
+      const res = await sendRequest(
+        "http://localhost:5011/port/getform",
+        "POST",
+        JSON.stringify({
+          id: localStorage.getItem("user"),
+        }),
+        { "Content-Type": "application/json" }
+      );
+      // console.log(res, "getformdata");
+      setportData(res);
+    };
+    Portfoilo();
 
-    port.portfolio.map((p) => {
-      save.push(dets.details.filter((det) => p.portfolio === det.Portfolio));
-    });
-    setMatch(save);
     // let newimg=[]
     // images.filter((data)=>
     //   data.name.lowercase() === props.details.platform
     // )
-  }, []);
+  }, [trans.tr]);
+  // console.log(portData);
+  // console.log(transData);
+  useEffect(() => {
+    let save = [];
+    portData.map((p) => {
+      save.push(transData.filter((det) => p.portfolio === det.portfolio));
+    });
+    // console.log(save, "yugyugyugiuiu");
+    setMatch(save);
+    trans.tr = false;
+    // console.log(trans.tr);
+  }, [transData]);
 
   const TableHandler = (e) => {
     e.preventDefault();
@@ -43,6 +85,7 @@ const Check = (props) => {
 
   return (
     <div className="portcard">
+      {console.log(props.details)}
       <img src={props.details.images[0].address} style={{ width: "60%" }}></img>
       <div className="portcard__content">
         <table className="portcardtable">
@@ -99,19 +142,20 @@ const Check = (props) => {
                 </tr>
               </thead>
               <tbody>
-                {match[val - 1].map((i) => {
-                  return (
-                    <tr>
-                      <td>{i.Portfolio}</td>
-                      <td>{i.Date}</td>
-                      <td>{i.Ticker}</td>
-                      <td>{i.action}</td>
-                      <td>{i.quantity}</td>
-                      <td>{i.price}</td>
-                      <td>{i.total}</td>
-                    </tr>
-                  );
-                })}
+                {match[val - 1] &&
+                  match[val - 1].map((i) => {
+                    return (
+                      <tr>
+                        <td>{i.portfolio}</td>
+                        <td>{i.date}</td>
+                        <td>{i.ticker}</td>
+                        <td>{i.action}</td>
+                        <td>{i.quantity}</td>
+                        <td>{i.price}</td>
+                        <td>{i.total}</td>
+                      </tr>
+                    );
+                  })}
               </tbody>
             </table>
           </div>
