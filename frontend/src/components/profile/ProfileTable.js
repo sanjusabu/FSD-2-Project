0da7bf81780e47aa90645f48ceaa49419c6dc123
PJ0710 <pranjay.g20@iiits.in>
2022-12-05 17:32:57 +0530

    profile ui

--- frontend/src/components/profile/ProfileTable.js
+++ frontend/src/components/profile/ProfileTable.js
@@ -4,18 +4,10 @@ import { useSelector } from "react-redux";
 
 const ProfileTable = () => {
   const dets = useContext(Detailscontext);
-  const [colors, setColor] = useState("");
   console.log(dets.details);
   const mode = useSelector((state) => state.darkMode);
   const { isdarkMode } = mode;
-  useEffect(() => {
-    //changing color of body with darkmode in useEffect
-    if (isdarkMode) {
-      setColor("black");
-    } else {
-      setColor("black");
-    }
-  }, [isdarkMode]);
+  
   return (
     <div className="row">
       <div className="col-md-12 mb-3">
@@ -25,24 +17,24 @@ const ProfileTable = () => {
               <table id="example" className="table table-striped data-table">
                 <thead>
                   <tr>
-                    <th style={{ color: colors }}>Portfolio</th>
-                    <th style={{ color: colors }}>Date of Transaction </th>
-                    <th style={{ color: colors }}>Ticker</th>
-                    <th style={{ color: colors }}>Action</th>
-                    <th style={{ color: colors }}>Quantity</th>
-                    <th style={{ color: colors }}>Price</th>
-                    <th style={{ color: colors }}>Total</th>
+                    <th>Portfolio</th>
+                    <th>Date of Transaction </th>
+                    <th>Ticker</th>
+                    <th>Action</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Total</th>
                   </tr>
 
                   {dets.details.map((i) => (
                     <tr>
-                      <td style={{ color: colors }}>{i.Portfolio}</td>
-                      <td style={{ color: colors }}>{i.Date}</td>
-                      <td style={{ color: colors }}>{i.Ticker}</td>
-                      <td style={{ color: colors }}>{i.action}</td>
-                      <td style={{ color: colors }}>{i.quantity}</td>
-                      <td style={{ color: colors }}>{i.price}</td>
-                      <td style={{ color: colors }}>{i.total}</td>
+                      <td>{i.Portfolio}</td>
+                      <td>{i.Date}</td>
+                      <td>{i.Ticker}</td>
+                      <td>{i.action}</td>
+                      <td>{i.quantity}</td>
+                      <td>{i.price}</td>
+                      <td>{i.total}</td>
                     </tr>
                   ))}
                 </thead>
