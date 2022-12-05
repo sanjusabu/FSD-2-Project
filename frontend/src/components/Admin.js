ed814484056aeefca030977af3334d509eedd203
Vikyath <lavanyasanka525@gmail.com>
2022-12-05 22:38:06 +0530

    Admin.js

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -2,26 +2,36 @@ import React, { useContext } from 'react';
 import { GlobalContext } from "./GlobalState";
 import NavBar from "./NavBar";
 import {
+  Navbar,
+  NavbarBrand,
+  Container,
   ListGroup,
   ListGroupItem,
   Button
 } from "reactstrap";
 
-<NavBar/>
+
  const UserList = () => {
   
   let { users, removeUser } = useContext(GlobalContext);
-  users = ["test", "test2", "test3"];
+  users = [{id: 1, name: "John Doe"}, {id: 2, name: "Jane Doe"}];
 
   return (
+    <>
+    <NavBar/>
+    <Navbar color="dark" dark>
+    <Container>
+      <NavbarBrand href="/">Admin</NavbarBrand>
+    </Container>
+  </Navbar>
     <ListGroup className="mt-4">
       {users.length > 0 ? (
         <>
           {users.map(user => (
             <ListGroupItem className="d-flex" key={user.id}>
-              <strong>{user.name}</strong>
-              <div className="ml-auto">
-                <Button onClick={() => removeUser(user.id)} color="danger">Delete</Button>
+              <strong className="mr-3">{user.name}</strong>
+              <div className = "ml-3">
+                <Button  onClick={() => removeUser(user.id)} color="danger">Delete</Button>
               </div>
             </ListGroupItem>
           ))}
@@ -30,7 +40,9 @@ import {
           <h4 className="text-center">No Users</h4>
         )}
     </ListGroup>
+    </>
   )
+        
 }
 
 export default UserList;
--- frontend/src/components/AppReducer.js
+++ frontend/src/components/AppReducer.js
@@ -7,25 +7,6 @@ const reducer = (state, action) => {
             return user.id !== action.payload;
           })
         }
-      case 'ADD_USER':
-        return {
-          ...state,
-          users: [action.payload, ...state.users]
-        }
-      case 'EDIT_USER':
-        const updateUser = action.payload;
-  
-        const updateUsers = state.users.map(user => {
-          if (user.id === updateUser.id) {
-            return updateUser;
-          }
-          return user;
-        })
-        return {
-          ...state,
-          users: updateUsers
-        }
-  
       default:
         return state;
     }
