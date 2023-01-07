45bdd901dca038d587c226bb70ee23b3ebad0e10
sanju <sanjusabu066@gmail.com>
2023-01-07 17:13:47 +0530

    hkj

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
       return;
     }
     const response = await sendRequest(
-      "http://localhost:5011/users/login",
+      "https://fsdproject2.onrender.com/users/login",
       "POST",
       JSON.stringify({
         email: emailValue,
