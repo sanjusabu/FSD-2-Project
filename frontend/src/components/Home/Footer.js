06b15ca9a74b7498e2ac019ce0bdf2f1e0c698bf
srikar <saisrikarreddy.k20@iiits.in>
2023-03-26 23:41:38 +0530

    footer changes

--- frontend/src/components/Home/Footer.js
+++ frontend/src/components/Home/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="footer-container">
       <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
+        <p className='footer-subscription-heading gdcgsuydcs'>
           A whole ecosystem of modern investment apps
           tailored to specific needs, built from the ground up
         </p>
@@ -22,12 +22,12 @@ function Footer() {
           <small className='website-rights'>SPD © 2023</small>
           <div className='social-icons'>
             <Link
-              className='social-icon-link facebook '
+              className='social-icon-link facebook'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
-              <i className='fab fa-facebook-f'/>
+              <i className='fab fa-facebook-f gdcgsuydcs'/>
             </Link>
             <Link
               className='social-icon-link instagram'
@@ -35,7 +35,7 @@ function Footer() {
               target='_blank'
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i class='fab fa-instagram gdcgsuydcs' />
             </Link>
             <Link
               class='social-icon-link youtube'
@@ -43,7 +43,7 @@ function Footer() {
               target='_blank'
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i class='fab fa-youtube gdcgsuydcs' />
             </Link>
             <Link
               class='social-icon-link twitter'
@@ -51,7 +51,7 @@ function Footer() {
               target='_blank'
               aria-label='Twitter'
             >
-              <i class='fab fa-twitter' />
+              <i class='fab fa-twitter gdcgsuydcs' />
             </Link>
             <Link
               class='social-icon-link twitter'
@@ -59,7 +59,7 @@ function Footer() {
               target='_blank'
               aria-label='LinkedIn'
             >
-              <i class='fab fa-linkedin' />
+              <i class='fab fa-linkedin gdcgsuydcs' />
             </Link>
           </div>
         </div>
