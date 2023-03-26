b891b7b098d328f5c03a4df02dbaa1f64e93f516
PJ0710 <pranjay.g20@iiits.in>
2023-03-26 23:29:00 +0530

    added csv import option

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,8 @@ import { Portfoliocontext } from "./context/portfolio-context";
 import { Provider } from "react-redux";
 import ForgotPassword from "./components/auth/ForgotPassword";
 import store from "./store/store";
+import CreateTransactions from "./components/createTransactions";
+import TransactionsCSV from "./components/TransactionsCSV";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -65,6 +67,8 @@ function App() {
         <Router>
           <Routes>
             <Route path="/transactions" element={<Transactions />}></Route>
+            <Route path="/manualtransactions" element={<CreateTransactions/>}></Route>
+            <Route path="/importtransactions" element={<TransactionsCSV/>}></Route>
             <Route path="/portfolio" element={<Portfolio />}></Route>
             <Route path="/profile" element={<Profile />}></Route>
             <Route path="/" element={<Profile />}></Route>
--- frontend/src/components/transactions.js
+++ frontend/src/components/transactions.js
@@ -1,8 +1,8 @@
-import CreateTransactions from "./createTransactions";
 import { useContext, useState, useEffect } from "react";
 import { Detailscontext } from "../context/details";
 import TempNavbar from "./tempNavbar.js";
 import TransactionsCSV from "./TransactionsCSV";
+import TransactionOption from "./TransactionOption";
 // import { Portfoliocontext } from "../context/portfolio-context";
 const Transactions = () => {
   const dets = useContext(Detailscontext);
@@ -17,8 +17,7 @@ const Transactions = () => {
   return (
     <>
       <TempNavbar />
-      <TransactionsCSV/>
-      <CreateTransactions takedetails={getdetails} />
+      <TransactionOption />
     </>
   );
 };
